39306cf5980a3938a83e83e503f2133c09a17608
DESKTOP-LTPLH83\USER <wnsgur1855@naver.com>
2023-10-12 13:54:06 +0900

    [feat] | 리액트 구조

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Router from './shared/router';
+
+function App() {
+  return <Router />;
+}
+
+export default App;
---
+++ src/pages/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+  return <div>리액트 코드짜기</div>;
+}
+
+export default Main;
